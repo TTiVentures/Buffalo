8fb522e083eec697835428dd880d5db26902ef5a
Alonso Muñoz <33006411+Devidence7@users.noreply.github.com>
2024-02-07 14:31:26 +0100

    .

--- src/TTI.Buffalo.ApiRest.Service/Program.cs
+++ src/TTI.Buffalo.ApiRest.Service/Program.cs
@@ -1,30 +1,26 @@
-using Buffalo.Sample;
-using Google.Cloud.Logging.Console;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Microsoft.VisualBasic;
-using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 using System.Text.Json.Serialization;
+using Buffalo.Sample;
+using Google.Cloud.Logging.Console;
+using Newtonsoft.Json;
 using TTI.Buffalo;
 using TTI.Buffalo.AmazonS3;
 using TTI.Buffalo.GoogleCloud;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
 if (builder.Environment.IsProduction())
 {
-    string? path = Environment.GetEnvironmentVariable("CONFIG_PATH");
+    var path = Environment.GetEnvironmentVariable("CONFIG_PATH");
     if (string.IsNullOrEmpty(path))
     {
         throw new ArgumentException("CONFIG_PATH in non Development Environment can NOT be null");
     }
 
-    string? cloud_env = Environment.GetEnvironmentVariable("CLOUD_ENV");
+    var cloud_env = Environment.GetEnvironmentVariable("CLOUD_ENV");
     if (!string.IsNullOrEmpty(cloud_env))
     {
         if (cloud_env == "AWS")
@@ -37,7 +33,7 @@ if (builder.Environment.IsProduction())
         }
     }
 
-    builder.Configuration.AddJsonFile(path, optional: false, reloadOnChange: true);
+    builder.Configuration.AddJsonFile(path, false, true);
 }
 
 builder.Services.AddControllers().AddJsonOptions(opts =>
@@ -46,27 +42,23 @@ builder.Services.AddControllers().AddJsonOptions(opts =>
     opts.JsonSerializerOptions.Converters.Add(enumConverter);
 });
 
-
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 PassportOptions passportOptions = new();
 builder.Configuration.GetSection("BuffaloSettings:Passport").Bind(passportOptions);
 
-
 S3Options amazonOptions = new();
 builder.Configuration.GetSection("BuffaloSettings:AWSCredentials").Bind(amazonOptions);
 
-Dictionary<string, object>? settings = builder.Configuration
-  .GetSection("BuffaloSettings:GoogleCredentialFile")
-  .Get<Dictionary<string, object>>();
-string json = JsonConvert.SerializeObject(settings);
+var settings = builder.Configuration
+    .GetSection("BuffaloSettings:GoogleCredentialFile")
+    .Get<Dictionary<string, object>>();
+var json = JsonConvert.SerializeObject(settings);
 
-string? system = builder.Configuration.GetSection("BuffaloSettings:AvailableSystem").Value;
-
+var system = builder.Configuration.GetSection("BuffaloSettings:AvailableSystem").Value;
 
 builder.Services.AddBuffalo(x =>
 {
-
     switch (system)
     {
         case "GCS":
@@ -94,74 +86,66 @@ builder.Services.AddBuffalo(x =>
 
 if (passportOptions.RequireAuthentication)
 {
-
     builder.Services.AddAuthentication("token")
-
-        // JWT tokens
         .AddJwtBearer("token", options =>
         {
-
+            // JWT tokens  
             options.Authority = passportOptions.Authority;
             options.Audience = passportOptions.Audience;
 
-            options.TokenValidationParameters = new TokenValidationParameters
+            options.TokenValidationParameters = new()
             {
                 ValidateAudience = true,
                 NameClaimType = "sub",
-                RoleClaimType = "role",
+                RoleClaimType = "role"
             };
 
             options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
 
             // if token does not contain a dot, it is a reference token
-            options.ForwardDefaultSelector = Selector.ForwardReferenceToken("introspection");
+            options.ForwardDefaultSelector = Selector.ForwardReferenceToken();
         })
-
-        // reference tokens
         .AddOAuth2Introspection("introspection", options =>
         {
+            // reference tokens  
             options.Authority = passportOptions.Authority;
 
             options.ClientId = passportOptions.ClientId;
             options.ClientSecret = passportOptions.ClientSecret;
 
-
             options.NameClaimType = "sub";
             options.RoleClaimType = "role";
-
         });
-
 }
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    options.SwaggerDoc("v1", new()
     {
         Version = "v1",
         Title = "Buffalo API",
         Description = "Buffalo allows Upload your Favorite Files via this Awesome Library for Objects",
-        Contact = new OpenApiContact
+        Contact = new()
         {
             Name = "Source",
-            Url = new Uri("https://github.com/TTiVentures/Buffalo")
+            Url = new("https://github.com/TTiVentures/Buffalo")
         },
-        License = new OpenApiLicense
+        License = new()
         {
             Name = "License",
-            Url = new Uri("https://github.com/TTiVentures/Buffalo/blob/main/LICENSE")
+            Url = new("https://github.com/TTiVentures/Buffalo/blob/main/LICENSE")
         }
     });
 
-    string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
 });
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if ((app.Configuration.GetValue<bool?>("BuffaloSettings:UseSwagger") ?? false) == true)
+if (app.Configuration.GetValue<bool?>("BuffaloSettings:UseSwagger") ?? false)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -170,14 +154,11 @@ if ((app.Configuration.GetValue<bool?>("BuffaloSettings:UseSwagger") ?? false) =
 app.UseAuthentication();
 app.UseAuthorization();
 
-if (passportOptions.RequireAuthentication)
-{ 
-    app.MapControllers().RequireAuthorization();
-}
-else
-{
-    app.MapControllers();
-}
+var controllers = app.MapControllers();
 
+if (passportOptions.RequireAuthentication)
+{
+    controllers.RequireAuthorization();
+}
 
 app.Run();
--- src/TTI.Buffalo.GoogleCloud/GoogleCloudStorage.cs
+++ src/TTI.Buffalo.GoogleCloud/GoogleCloudStorage.cs
@@ -1,175 +1,164 @@
-﻿using Google;
+﻿using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text.Json;
+using Google;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Auth.OAuth2.Responses;
 using Google.Apis.Download;
 using Google.Cloud.Storage.V1;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text.Json;
 using TTI.Buffalo.Models;
-using static System.Reflection.Metadata.BlobBuilder;
+using Object = Google.Apis.Storage.v1.Data.Object;
 
-namespace TTI.Buffalo.GoogleCloud
+namespace TTI.Buffalo.GoogleCloud;
+
+public class GoogleCloudStorage : IStorage, IDisposable
 {
-    public class GoogleCloudStorage : IStorage, IDisposable
-	{
-		private readonly StorageClient _storageClient;
-		private readonly string _bucketName;
+    private readonly string _bucketName;
+    private readonly StorageClient _storageClient;
 
-		public GoogleCloudStorage(IOptions<GCSOptions> options)
-		{
-			GoogleCredential? googleCredential = GoogleCredential.FromJson(options.Value.JsonCredentialsFile);
-			_storageClient = StorageClient.Create(googleCredential);
-			_bucketName = options.Value.StorageBucket ?? "default";
-		}
+    public GoogleCloudStorage(IOptions<GCSOptions> options)
+    {
+        var googleCredential = GoogleCredential.FromJson(options.Value.JsonCredentialsFile);
+        _storageClient = StorageClient.Create(googleCredential);
+        _bucketName = options.Value.StorageBucket ?? "default";
+    }
 
-		public async Task<string> UploadFileAsync(IFormFile file, string fileNameForStorage, 
-			AccessLevels accessLevel, ClaimsPrincipal? user, RequiredClaims? requiredClaims)
-		{
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
 
-            string? userId = null;
-            if (user != null && user.Identity != null)
-            {
-                userId = user.Identity.Name;
-            }
-
-            var al = MD5.Create();
-            using var cs = new CryptoStream(file.OpenReadStream(), al, CryptoStreamMode.Read);
-
-			Dictionary<string, string> meta = new ()
-					{
-						{ MetadataConst.BUFFALO_ACCESS_MODE, accessLevel.ToString() },
-						{ MetadataConst.BUFFALO_USER_ID, userId ?? "" },
-						{ MetadataConst.BUFFALO_FILENAME, file.FileName }
-					};
-
-			if (accessLevel == AccessLevels.CLAIMS)
-			{
-				if (requiredClaims != null)
-				{
-                    meta.Add(MetadataConst.BUFFALO_REQUIRED_CLAIMS, JsonSerializer.Serialize(requiredClaims));
-                }
-				else
-				{
-					throw new ArgumentException("RequiredClaims must not be empty in CLAIMS level access");
-				}
-				
-			}
-
-            Google.Apis.Storage.v1.Data.Object? obj = new()
-			{
-				Bucket = _bucketName,
-				Name = fileNameForStorage,
-				ContentType = file.ContentType ?? MimeTypeTool.GetMimeType(file.FileName),
-				Metadata = meta
-			};
-
-			Google.Apis.Storage.v1.Data.Object dataObject =
-				await _storageClient.UploadObjectAsync(obj, cs, new UploadObjectOptions
-				{
-					PredefinedAcl = accessLevel == AccessLevels.PUBLIC ? PredefinedObjectAcl.PublicRead : PredefinedObjectAcl.Private
-				});
-
-			return "https://storage.cloud.google.com/" + dataObject.Bucket + "/" + dataObject.Name;
-		}
-
-		public async Task DeleteFileAsync(Guid id, ClaimsPrincipal? user)
-		{
-			try
-			{
-				Google.Apis.Storage.v1.Data.Object? obj = _storageClient.GetObject(_bucketName, id.ToString());
-
-				if (SecurityTool.VerifyAccess(user, obj.Metadata[MetadataConst.BUFFALO_USER_ID], 
-					obj.Metadata[MetadataConst.BUFFALO_ACCESS_MODE], obj.Metadata[MetadataConst.BUFFALO_REQUIRED_CLAIMS]) == false)
-				{
-					throw new UnauthorizedAccessException("Unauthorized access to PROTECTED resource");
-				}
-
-				await _storageClient.DeleteObjectAsync(_bucketName, id.ToString());
-
-			}
-			catch (GoogleApiException ex)
-			{
-				if (ex.Error.Code == 404)
-				{
-					throw new FileNotFoundException(ex.Message);
-				}
-				else
-				{
-					throw new ApplicationException(ex.Message);
-
-				}
-			}
-		}
-
-		public void Dispose()
-		{
-			GC.SuppressFinalize(this);
-		}
-
-		public async Task<FileData> RetrieveFileAsync(Guid id, ClaimsPrincipal? user)
-		{
-			try
-			{
-				Google.Apis.Storage.v1.Data.Object? obj = await _storageClient.GetObjectAsync(_bucketName, id.ToString());
-
-				if (SecurityTool.VerifyAccess(user, obj.Metadata[MetadataConst.BUFFALO_USER_ID], 
-					obj.Metadata[MetadataConst.BUFFALO_ACCESS_MODE], obj.Metadata[MetadataConst.BUFFALO_REQUIRED_CLAIMS]) == false)
-				{
-					throw new UnauthorizedAccessException("Unauthorized access to PROTECTED resource");
-				}
-
-				MemoryStream? memoryStream = new();
-
-				// IDownloadProgress defined in Google.Apis.Download namespace
-				Progress<IDownloadProgress>? progress = new(
-					p => Console.WriteLine($"bytes: {p.BytesDownloaded}, status: {p.Status}")
-				);
-
-				// Download source object from bucket to local file system
-				_ = await _storageClient.DownloadObjectAsync(_bucketName, id.ToString(), memoryStream, null, default, progress);
-
-				memoryStream.Position = 0;
-
-				return new FileData(memoryStream, obj.Metadata[MetadataConst.BUFFALO_FILENAME], obj.ContentType);
-
-			}
-			catch (GoogleApiException ex)
-			{
-				if (ex.Error.Code == 404)
-				{
-					throw new FileNotFoundException(ex.Message);
-				}
-				else
-				{
-					throw new ApplicationException(ex.Message);
-				}
-			}
-			catch (TokenResponseException)
-			{
-				throw new UnauthorizedAccessException("Provided Cloud Storage credentials do not have access to this resource.");
-			}
-		}
-        public async Task<ObjectList> RetrieveFileListAsync()
+    public async Task<string> UploadFileAsync(IFormFile file, string fileNameForStorage,
+        AccessLevels accessLevel, ClaimsPrincipal? user, RequiredClaims? requiredClaims)
+    {
+        string? userId = null;
+        if (user != null && user.Identity != null)
         {
-
-
-			var result = _storageClient.ListObjectsAsync(_bucketName);
-			int total = 0;
-
-			var response = new ObjectList();
-
-            await foreach (var blob in result)
-            {
-				response.Objects.Add(blob.Name);
-				++total;
-            }
-			response.Total = total;
-
-			return response;
-
+            userId = user.Identity.Name;
         }
+
+        var al = MD5.Create();
+        using var cs = new CryptoStream(file.OpenReadStream(), al, CryptoStreamMode.Read);
+
+        Dictionary<string, string> meta = new()
+        {
+            { MetadataConst.BUFFALO_ACCESS_MODE, accessLevel.ToString() },
+            { MetadataConst.BUFFALO_USER_ID, userId ?? "" },
+            { MetadataConst.BUFFALO_FILENAME, file.FileName }
+        };
+
+        if (accessLevel == AccessLevels.CLAIMS)
+        {
+            if (requiredClaims != null)
+            {
+                meta.Add(MetadataConst.BUFFALO_REQUIRED_CLAIMS, JsonSerializer.Serialize(requiredClaims));
+            }
+            else
+            {
+                throw new ArgumentException("RequiredClaims must not be empty in CLAIMS level access");
+            }
+        }
+
+        Object? obj = new()
+        {
+            Bucket = _bucketName,
+            Name = fileNameForStorage,
+            ContentType = file.ContentType ?? MimeTypeTool.GetMimeType(file.FileName),
+            Metadata = meta
+        };
+
+        var dataObject =
+            await _storageClient.UploadObjectAsync(obj, cs, new()
+            {
+                PredefinedAcl = accessLevel == AccessLevels.PUBLIC ? PredefinedObjectAcl.PublicRead : PredefinedObjectAcl.Private
+            });
+
+        return "https://storage.cloud.google.com/" + dataObject.Bucket + "/" + dataObject.Name;
+    }
+
+    public async Task DeleteFileAsync(Guid id, ClaimsPrincipal? user)
+    {
+        try
+        {
+            var obj = _storageClient.GetObject(_bucketName, id.ToString());
+
+            if (SecurityTool.VerifyAccess(user, obj.Metadata[MetadataConst.BUFFALO_USER_ID],
+                    obj.Metadata[MetadataConst.BUFFALO_ACCESS_MODE], obj.Metadata[MetadataConst.BUFFALO_REQUIRED_CLAIMS]) == false)
+            {
+                throw new UnauthorizedAccessException("Unauthorized access to PROTECTED resource");
+            }
+
+            await _storageClient.DeleteObjectAsync(_bucketName, id.ToString());
+        }
+        catch (GoogleApiException ex)
+        {
+            if (ex.Error.Code == 404)
+            {
+                throw new FileNotFoundException(ex.Message);
+            }
+
+            throw new ApplicationException(ex.Message);
+        }
+    }
+
+    public async Task<FileData> RetrieveFileAsync(Guid id, ClaimsPrincipal? user)
+    {
+        try
+        {
+            var obj = await _storageClient.GetObjectAsync(_bucketName, id.ToString());
+
+            if (SecurityTool.VerifyAccess(user, obj.Metadata[MetadataConst.BUFFALO_USER_ID],
+                    obj.Metadata[MetadataConst.BUFFALO_ACCESS_MODE], obj.Metadata[MetadataConst.BUFFALO_REQUIRED_CLAIMS]) == false)
+            {
+                throw new UnauthorizedAccessException("Unauthorized access to PROTECTED resource");
+            }
+
+            MemoryStream? memoryStream = new();
+
+            // IDownloadProgress defined in Google.Apis.Download namespace
+            Progress<IDownloadProgress>? progress = new(
+                p => Console.WriteLine($"bytes: {p.BytesDownloaded}, status: {p.Status}")
+            );
+
+            // Download source object from bucket to local file system
+            _ = await _storageClient.DownloadObjectAsync(_bucketName, id.ToString(), memoryStream, null, default, progress);
+
+            memoryStream.Position = 0;
+
+            return new(memoryStream, obj.Metadata[MetadataConst.BUFFALO_FILENAME], obj.ContentType);
+        }
+        catch (GoogleApiException ex)
+        {
+            if (ex.Error.Code == 404)
+            {
+                throw new FileNotFoundException(ex.Message);
+            }
+
+            throw new ApplicationException(ex.Message);
+        }
+        catch (TokenResponseException)
+        {
+            throw new UnauthorizedAccessException("Provided Cloud Storage credentials do not have access to this resource.");
+        }
+    }
+
+    public async Task<ObjectList> RetrieveFileListAsync()
+    {
+        var result = _storageClient.ListObjectsAsync(_bucketName);
+        var total = 0;
+
+        var response = new ObjectList();
+
+        await foreach (var blob in result)
+        {
+            response.Objects.Add(blob.Name);
+            ++total;
+        }
+
+        response.Total = total;
+
+        return response;
     }
 }
